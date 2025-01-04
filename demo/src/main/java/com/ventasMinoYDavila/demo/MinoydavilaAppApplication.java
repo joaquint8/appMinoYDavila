98afb29ffbee3fbdc4e0286c9c7d04255422cbd4
joaco <joaco@DESKTOP-KCPCDBH>
2025-01-04 16:40:03 -0300

    Initial commit

---
+++ demo/src/main/java/com/ventasMinoYDavila/demo/MinoydavilaAppApplication.java
@@ -0,0 +1,13 @@
+package com.ventasMinoYDavila.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinoydavilaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinoydavilaAppApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/ventasMinoYDavila/demo/MinoydavilaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventasMinoYDavila.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinoydavilaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
